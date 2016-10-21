f3bc01d95962055321db7cef745a01bc87483e86
hzfanfei <hzfanfei@corp.netease.com>
2016-10-21 11:23:47 +0800

    first commit

---
+++ realm/realmDemo/realmDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  realmDemo
+//
+//  Created by FanFamily on 16/10/21.
+//  Copyright © 2016年 niuniu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
